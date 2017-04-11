8a34d5be568d129b98013015e0d7d09a8cf2f16c
drworm75 <dwgames@gmail.com>
2017-04-10 21:45:22 -0500

    Project meets specifcations

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,14 +9,47 @@ $.ajax('./db/dinosaurs.json').done(function(data) {
 
 function makeDom(myArrayToPrint) {
 	var myDomString = "";
+	myDomString += `<div class="row">`
 	for (var i = 0; i < myArrayToPrint.length; i++) {
-		myDomString += `<div class="dinoCard">`;
+		myDomString += `<div class="dinoCard col-md-4">`;
 		myDomString += `<header><h1>${myArrayToPrint[i].type}</h1></header>`;
 		myDomString += `<section><img src="${myArrayToPrint[i].img}">`;
 		myDomString += `<p class="bio">${myArrayToPrint[i].bio}</p></section>`;
 		myDomString += `<footer><h4>${myArrayToPrint[i].info}</h4></footer>`;
 		myDomString += `</div>`
+	if (((i + 1) % 3) === 0) {
+		myDomString += `</div>`
+		$('#dinosaurs').append(myDomString);
+			myDomString = "";
+			myDomString += `<div class="row">`			
+		}
 	}
-	$('#dinosaurs').html(myDomString);
 
 }
+
+$('#dinosaurs').on ('click', '.dinoCard', function(e) {
+    $('.dinoCard').removeClass("dottedBorder");
+    $(this).addClass("dottedBorder");
+    $("#textbox").val("").focus(); 
+    $("#textbox").val($('.dottedBorder').find('p.bio').text()); 
+});
+
+$('#textbox').keyup(mirrorText);
+
+function mirrorText(e) {
+	var selectedCard = $('.dottedBorder');
+	var bioTyped = $('#textbox').val();
+	var bio = $('.dottedBorder').find('p.bio');
+	bio.html(bioTyped);
+
+	if (e.keyCode === 13) {
+		$('#textbox').val('');
+	}
+}
+
+
+
+
+
+
+
